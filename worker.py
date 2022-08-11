fe5c6abde9413611c98a129c2aed09f7f0716ba7
PureSolutions1 <95774109+PureSolutions1@users.noreply.github.com>
2022-08-11 11:54:25 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,24 +7,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-#conn = redis.from_url(redis_url)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+conn = redis.from_url(redis_url)
 
-redis_host = os.environ.get('REDIS_HOST', 'localhost')    
-# Channel layer definitions
-# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
-CHANNEL_LAYERS = {
-    "default": {
-        # This example app uses the Redis channel layer implementation asgi_redis
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(redis_host, 6379)],
-        },
-        "ROUTING": "multichat.routing.channel_routing",
-    },
-}
 
-#if __name__=='__main__':
-    #with Connection(conn):
-       # worker = Worker(map(Queue, listen))
-       # worker.work()
+if __name__=='__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
