5a46aef45a3d226ef2a35f9564035ed1f0f834e1
PureSolutions1 <95774109+PureSolutions1@users.noreply.github.com>
2022-08-11 11:53:19 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -24,7 +24,7 @@ CHANNEL_LAYERS = {
     },
 }
 
-if __name__=='__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+#if __name__=='__main__':
+    #with Connection(conn):
+       # worker = Worker(map(Queue, listen))
+       # worker.work()
