b25a4b2e36d6693108275886324d33da642d4ca1
PureSolutions1 <95774109+PureSolutions1@users.noreply.github.com>
2022-08-11 11:52:55 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,7 +1,5 @@
 import os
 import sys
-sudo apt-get install redis-server
-service redis-server start
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages')
 import redis
 from rq import Worker, Queue, Connection
@@ -9,9 +7,22 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#conn = redis.from_url(redis_url)
 
+redis_host = os.environ.get('REDIS_HOST', 'localhost')    
+# Channel layer definitions
+# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
+CHANNEL_LAYERS = {
+    "default": {
+        # This example app uses the Redis channel layer implementation asgi_redis
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(redis_host, 6379)],
+        },
+        "ROUTING": "multichat.routing.channel_routing",
+    },
+}
 
 if __name__=='__main__':
     with Connection(conn):
