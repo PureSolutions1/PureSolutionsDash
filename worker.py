732a485deefcfb0ef95a34baadd1accb0fc6945a
PureSolutions1 <95774109+PureSolutions1@users.noreply.github.com>
2022-08-11 11:01:39 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,13 +1,20 @@
 import os
 import sys
+import urlparse
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages')
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#conn = redis.from_url(redis_url)
+
+redis_url = os.getenv('REDISTOGO_URL')
+
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 
 if __name__=='__main__':
