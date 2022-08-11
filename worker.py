75302a5f9c98d4eaec8fb564933107bfbd70f8f6
PureSolutions1 <95774109+PureSolutions1@users.noreply.github.com>
2022-08-11 11:52:01 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+sudo apt-get install redis-server
+service redis-server start
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages')
 import redis
 from rq import Worker, Queue, Connection
